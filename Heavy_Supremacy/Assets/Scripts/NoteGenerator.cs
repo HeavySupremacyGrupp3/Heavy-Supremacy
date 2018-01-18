b7dbc6a4d16b7a9c10a4a3d1da0548a29c6b0589
Arvid <arvid.enoch@hotmail.com>
2018-01-18 16:44:12 +0100

    NotGenerator

---
+++ Heavy_Supremacy/Assets/Scripts/NoteGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteGenerator : MonoBehaviour
+{
+    public AudioClip AudioClip;
+    public AudioSource AudioSource;
+    public float UpdateInterval = 0.1f;
+    public int sampleDataLength = 1024;
+
+    private float clipTime = 0f;
+    private float clipVolume;
+    private float[] clipSampleData;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+
+        clipTime += Time.deltaTime;
+        if (clipTime >= UpdateInterval)
+        {
+            clipTime = 0f;
+            AudioSource.clip.GetData(clipSampleData, AudioSource.timeSamples); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+            clipVolume = 0f;
+            foreach (float sample in clipSampleData)
+            {
+                clipVolume += Mathf.Abs(sample);
+            }
+            clipVolume /= sampleDataLength;
+            Debug.Log(clipVolume);
+        }
+    }
+
+    void Initialize()
+    {
+        clipSampleData = new float[sampleDataLength];
+        
+    }
+}
