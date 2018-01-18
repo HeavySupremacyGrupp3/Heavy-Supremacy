09f6eb2b2fe426e99c6cedc93dbec049d4337dd8
unknown <Administratr@itgsko3-LS2R9027YEC.learnet.se>
2018-01-18 15:05:57 +0100

    added Stats-class
    basic stats

---
+++ Heavy_Supremacy/Assets/Scripts/Stats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+	float amount;
+
+	void Start ()
+	{
+			
+	}
+		
+	void Update ()
+	{
+			
+	}		
+
+	public void setAmount(float a)
+	{
+		amount=a;
+	}
+
+	public float getAmount()
+	{
+		return amount;
+	}
+
+	public void showProgressBar()
+	{
+		
+	}
+}
