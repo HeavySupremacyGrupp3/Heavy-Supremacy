ec086119e9eeb57f998af91b326e2d0135af80a3
unknown <Administratr@itgsko3-LS2R9027YEC.learnet.se>
2018-01-19 13:26:55 +0100

    moneyText

---
+++ Assets/Scripts/moneyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moneyScript : MonoBehaviour {
+
+	public int Money;
+	private Text t;
+
+	void Start () 
+	{
+		Money = 0;
+		t=GetComponent<Text> ();
+	}
+
+	void Update () 
+	{
+		t.text = "Money: " + Money;
+	}
+
+	public void addMoney(int value)
+	{
+		Money += value;
+	}
+
+	public void setMoney(int value)
+	{
+		Money = value;
+	}
+
+	public int getMoney()
+	{
+		return Money;
+	}
+}
