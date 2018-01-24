cb737ecd44adbac0ae7cdb7237b9dc9a4efc17d0
Arvid <arvid.enoch@hotmail.com>
2018-01-24 09:57:38 +0100

    Added Lightning to practice scene. Added my MixedFX pack.

---
+++ Assets/MixedFXPack/README.txt
@@ -0,0 +1,6 @@
+Import the "Particle Systems" package from the Unity standard assets.
+Only the textures in the "textures" folder from that package should be required but to ensure
+that everything will work as expected. Though if you want to skip the tinkering of what to import
+and what not to import from the "particle systems" package you can import the whole thing.
+
+Regards, Wolmer
---
+++ Assets/MixedFXPack/Scripts/PreviewNextScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreviewNextScript : MonoBehaviour {
+
+    GameObject particleSystems;
+    int childIndex = 0;
+
+    Text nameText;
+
+	void Start ()
+    {
+        nameText = GameObject.Find("Name").GetComponent<Text>();
+
+		particleSystems = GameObject.Find("ParticleSystems");
+        if (particleSystems == null)
+            particleSystems = this.gameObject;
+
+        for (int i = 1; i < particleSystems.transform.childCount; i++)
+        {
+            particleSystems.transform.GetChild(i).gameObject.SetActive(false);
+        }
+	}
+	
+	void Update ()
+    {
+        nameText.text = particleSystems.transform.GetChild(childIndex).name;
+	}
+
+    public void NextParticleSystem(bool forward)
+    {
+        particleSystems.transform.GetChild(childIndex).gameObject.SetActive(false);
+
+        if (forward)
+            childIndex++;
+        else if (!forward)
+            childIndex--;
+
+        if (childIndex > particleSystems.transform.childCount - 1)
+            childIndex = 0;
+        else if (childIndex < 0)
+            childIndex = particleSystems.transform.childCount - 1;
+
+        particleSystems.transform.GetChild(childIndex).gameObject.SetActive(true);
+    }
+
+    public void Replay()
+    {
+        particleSystems.transform.GetChild(childIndex).gameObject.SetActive(false);
+        particleSystems.transform.GetChild(childIndex).gameObject.SetActive(true);
+    }
+}
