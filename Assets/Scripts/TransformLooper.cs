0e4c001a0c30ca89199e81ea0826b03510c07828
photofisken <julialuma@hotmail.com>
2018-01-24 16:02:48 +0100

    Adding rullband
    (Sorry if I'm messing up your code Arvid! It's not done yet)

--- Assets/Scripts/TransformLooper.cs
+++ Assets/Scripts/TransformLooper.cs
@@ -11,6 +11,9 @@ public class TransformLooper : MonoBehaviour
     private float posTimer = 0;
     private Vector2 startPos;
 
+    [SerializeField]
+    private GameObject travelator;
+
     private void Start()
     {
         startPos = transform.localPosition;
@@ -19,10 +22,20 @@ public class TransformLooper : MonoBehaviour
 
     void Update()
     {
+        NewTravelator();
         posTimer += Time.deltaTime * Speed;
         if (posTimer >= 1)
             posTimer = 0;
 
         transform.localPosition = Vector2.Lerp(startPos, TargetPosition, posTimer);
+    }
+
+    private void NewTravelator()
+    {
+        if (transform.position.x == 0)
+        {
+            travelator = Instantiate(travelator, new Vector3(startPos.x -19.43f, startPos.y), Quaternion.identity);
+            //-19.43
+        }
     }
 }
