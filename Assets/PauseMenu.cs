6ecb3995850eb287af680441240087b41bb66923
Julia L <julialuma@hotmail.com>
2018-02-05 16:06:53 +0100

    Basic pause menu
    press escape to trigger

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool paused = false;
+    public GameObject pauseMenu;
+	
+
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused == false)
+                Pause();
+        }
+	}
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        paused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        paused = false;
+        Time.timeScale = 1f;
+    }
+}
+
