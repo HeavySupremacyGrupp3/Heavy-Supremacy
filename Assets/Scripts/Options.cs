12dfb35f79fccd5484fe22e80038f97f03f4cb1c
Julia L <julialuma@hotmail.com>
2018-02-06 11:31:42 +0100

    Pause and options menu

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Options : MonoBehaviour {
+
+    public AudioMixer am;
+
+	public void SetVolume(float volume)
+    {
+
+        am.SetFloat("MainVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+
+        am.SetFloat("SFXVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+
+        am.SetFloat("MusicVolume", volume);
+    }
+
+}
