435258d94bcb60d2fe1e3917b1bb6001d3a607a6
PiratarrRn <linneathebeast@hotmail.com>
2018-02-07 15:09:30 +0100

    workTutorial

---
+++ Assets/workTutorialManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class workTutorialManager : MonoBehaviour {
+
+
+	public void showText()
+	{
+		
+	}
+	
+	public void ToggleGameObject(GameObject target)
+    {
+		Debug.Log("AAAAAAAAARGHHH!");
+        target.SetActive(!target.active);
+    }
+}
