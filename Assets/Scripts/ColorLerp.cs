6884d7606bf8b9eef25f5153ecec39c067a12b4b
Arvid <arvid.enoch@hotmail.com>
2018-02-09 16:06:00 +0100

    Nya stat-bars (WIP) och en Shop mapp i Graphics

--- Assets/Scripts/ColorLerp.cs
+++ Assets/Scripts/ColorLerp.cs
@@ -5,6 +5,9 @@ public class ColorLerp : MonoBehaviour
     public float TransitionTime = 1;
     public Color StartColor = Color.white;
     public Color EndColor = Color.white;
+    public bool Lerp;
+    public bool Reverse;
+
 
     private SpriteRenderer spriteRenderer;
     private TextMesh textMesh;
@@ -25,17 +28,27 @@ public class ColorLerp : MonoBehaviour
 
     void Update()
     {
-        UpdateTransition();
+        if(Lerp)
+            UpdateTransition();
     }
 
     void UpdateTransition()
     {
-        timer -= Time.deltaTime;
+        if (Reverse && timer >= 0)
+            timer -= Time.deltaTime;
+        else if (timer <= 1 && !Reverse)
+            timer += Time.deltaTime;
+
         currentColor = Color.Lerp(EndColor, StartColor, timer / TransitionTime);
 
         if (spriteRenderer != null)
             spriteRenderer.color = currentColor;
         if (textMesh != null)
             textMesh.color = currentColor;
+    }
+
+    public void ReverseLerp()
+    {
+        Reverse = !Reverse;
     }
 }
