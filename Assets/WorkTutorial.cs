6ada2a19ae5e680ff18f40069914064cbb9c5574
PiratarrRn <linneathebeast@hotmail.com>
2018-02-16 15:46:33 +0100

    gjorde worktutorial mindre hårdkodad

--- Assets/WorkTutorial.cs
+++ Assets/WorkTutorial.cs
@@ -9,12 +9,14 @@ public class WorkTutorial : MonoBehaviour
 	public GameObject[] HighlightedObjects;
     public string[] AnimationTriggers;
     public KeyCode[] Buttons;
+	public uint HighlightInterval;
 
     private uint currentIndex = 0;
     private uint sections = 0;
     private Animator anim;
     private bool isAllowedClick = false;
     private KeyCode nextButton;
+	private uint A, B;
 	
 	void Start ()
     {
@@ -25,18 +27,21 @@ public class WorkTutorial : MonoBehaviour
 	
 	void highlightCurrentObject()
 	{
+		A=currentIndex/HighlightInterval;
+		B=(currentIndex+HighlightInterval)/HighlightInterval;
+		
 		Component[] spritedChildren;
 		if(currentIndex>0)		
 		{
-			HighlightedObjects[currentIndex/2].GetComponent<SpriteRenderer>().sortingOrder=1;
-			spritedChildren = HighlightedObjects[currentIndex/2].GetComponentsInChildren<SpriteRenderer>();		
+			HighlightedObjects[A].GetComponent<SpriteRenderer>().sortingOrder=1;
+			spritedChildren = HighlightedObjects[A].GetComponentsInChildren<SpriteRenderer>();		
 			
 			foreach (SpriteRenderer spriteRenderer in spritedChildren)
 				spriteRenderer.sortingOrder=1;	
 		}	
 		
-		HighlightedObjects[(currentIndex+2)/2].GetComponent<SpriteRenderer>().sortingOrder=200;
-		spritedChildren = HighlightedObjects[(currentIndex+2)/2].GetComponentsInChildren<SpriteRenderer>();			
+		HighlightedObjects[B].GetComponent<SpriteRenderer>().sortingOrder=200;
+		spritedChildren = HighlightedObjects[B].GetComponentsInChildren<SpriteRenderer>();			
 			
 		foreach (SpriteRenderer spriteRenderer in spritedChildren)
 			spriteRenderer.sortingOrder=200;	
@@ -55,7 +60,7 @@ public class WorkTutorial : MonoBehaviour
 		
 		Debug.Log("Rock me like a hurricane "+currentIndex);
 		
-		if(currentIndex%2==0 && ((currentIndex+2)/2)<HighlightedObjects.Length)
+		if(currentIndex%HighlightInterval==0 && (B)<HighlightedObjects.Length)
 			highlightCurrentObject();
     }
 
