2d2b5d2e43607f9353366973bbc1b42899622d8f
PiratarrRn <linneathebeast@hotmail.com>
2018-02-20 13:20:12 +0100

    tutorialsmeny

---
+++ Assets/Scripts/TutorialMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class TutorialMenuScript : MonoBehaviour {
+
+	public SceneTransitionScript SceneTransition;
+
+	public void LoadHUB()
+    {
+		SceneTransition.StartTransition("HUBScene");
+		//SceneManager.LoadScene("HUBScene");
+    }
+	
+	public void LoadWorkTutorial()
+	{
+		SceneTransition.StartTransition("WorkTutorialScene");
+		Debug.Log("Loading work - tutorial");
+	}
+	
+	public void LoadPracticeTutorial()
+	{
+	}
+	
+	public void LoadWork()
+    {
+		SceneTransition.StartTransition("WorkScene");
+    }
+}
