896246decf83f013e6cd8a168f5349a1f29ab652
photofisken <julialuma@hotmail.com>
2018-02-21 17:12:05 +0100

    Cleaning the workscene

--- Assets/Scripts/MachineBehaviour.cs
+++ Assets/Scripts/MachineBehaviour.cs
@@ -21,15 +21,13 @@ public class MachineBehaviour : MonoBehaviour {
     private GameObject machine3;
 
     public float spacing;
-
-    [SerializeField]
-    private float lerpTime = 2f;
+    public Transform checkpoint;
 
     void Start()
     {
         //Instantiate three machines at their positions											y=5.8;
-        machine1 = Instantiate(machinesToSpawn[0], new Vector3(transform.position.x - spacing, 2.5f), Quaternion.identity);
-        machine2 = Instantiate(machinesToSpawn[1], new Vector3(transform.position.x, 2.5f), Quaternion.identity);
+        machine1 = Instantiate(machinesToSpawn[0], new Vector3(transform.position.x - spacing, 4f), Quaternion.identity);
+        machine2 = Instantiate(machinesToSpawn[1], new Vector3(transform.position.x, 4f), Quaternion.identity);
         machine3 = Instantiate(machinesToSpawn[2], new Vector3(transform.position.x + spacing, 5f), Quaternion.identity);
 																								//6.8
         //machine1.GetComponent<MachineProperties>().type = 0;
