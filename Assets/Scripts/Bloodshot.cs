fa0e32bfef3c6617aadb1d54dfe0d86593461e6b
EineKleinehiTo <35545075+EineKleinehiTo@users.noreply.github.com>
2018-02-28 15:14:19 +0100

    Bloodshot gör mer än en sak nu. Newbs

--- Assets/Scripts/Bloodshot.cs
+++ Assets/Scripts/Bloodshot.cs
@@ -7,15 +7,22 @@ public class Bloodshot : MonoBehaviour
     [Range(0f, 1f)]
     public float Progress = 0f;
     public Color StartColor, EndColor;
-    private ParticleSystem.MainModule Particles;
-    
-	void Start ()
+    public int StartAmountOfParticles, EndAmountOfParticles;
+    private ParticleSystem Particles;
+    private ParticleSystem.MainModule ParticlesMainModule;
+    private ParticleSystem.EmissionModule ParticlesEmissionModule;
+
+    void Start()
     {
-        Particles = GetComponent<ParticleSystem>().main;
-	}
-	
-	void Update ()
+        Particles = GetComponent<ParticleSystem>();
+        ParticlesMainModule = Particles.main;
+        ParticlesEmissionModule = Particles.emission;
+    }
+
+
+    void Update ()
     {
-        Particles.startColor = Color.Lerp(StartColor, EndColor, Progress);
+        ParticlesMainModule.startColor = Color.Lerp(StartColor, EndColor, Progress);
+        ParticlesEmissionModule.rateOverTime = StartAmountOfParticles + Progress * (EndAmountOfParticles - StartAmountOfParticles);
 	}
 }
