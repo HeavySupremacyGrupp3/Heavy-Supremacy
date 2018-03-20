4fb235170694e7bcba783c874ccffcaf35aba425
photofisken <julialuma@hotmail.com>
2018-03-20 12:01:04 +0100

    Fixed the intro screen lightning

---
+++ Assets/Scripts/ChangeLocalScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeLocalScale : MonoBehaviour {
+
+    public float largeWidth;
+    public float largeHeight;
+    public float smallWidth;
+    public float smallHeight;
+    public Button thisButton;
+
+    public void LargeSize()
+    {
+        thisButton.GetComponent<RectTransform>().sizeDelta = new Vector2(largeWidth, largeHeight);
+
+    }
+
+    public void SmallSize()
+    {
+        thisButton.GetComponent<RectTransform>().sizeDelta= new Vector2(smallWidth, smallHeight);
+
+    }
+
+}
